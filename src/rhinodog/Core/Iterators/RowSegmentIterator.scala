27dfc3a6117ed3aadccce75e9d2f16b69a830d71
kk00ss <acc4konstantin@gmail.com>
2016-06-02 14:27:54 +0300

    added license

--- src/rhinodog/Core/Iterators/RowSegmentIterator.scala
+++ src/rhinodog/Core/Iterators/RowSegmentIterator.scala
@@ -1,3 +1,16 @@
+// Copyright 2016 Konstantin Voznesenskiy
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package rhinodog.Core.Iterators
 
 import rhinodog.Core.Definitions._
@@ -57,4 +70,3 @@ class RowSegmentIterator
         else return -1
     }
 }
-
